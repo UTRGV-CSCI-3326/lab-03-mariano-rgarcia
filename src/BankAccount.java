8ed431db808e3df5670eea064d854ba5f7ac09d8
mariano-rgarcia <mariano.garcia04@utrgv.edu>
2025-02-02 03:01:17 +0000

    Final lab3

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,6 @@
-
+public class BankAccount{
+    public static void main(String[] args){
+        double acc = 1961.90; // bank account amount
+        System.out.println("This is your balance "+acc);
+    }
+}
